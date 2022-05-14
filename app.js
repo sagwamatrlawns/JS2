e67ca371f0d249965ab6f2375e628d1c1376855a
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-14 23:08:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// Variables (변수))
+// JS에서는 변수명 사이에 공백을 넣을 수 없다.
+// 공백대신 다음 글자를 대문자로 입력해서 구분하는 것이 일반적이다.
+
 const a = 5;
 const b = 2;
+const myName = "KJ";
 
 console.log(a + b);
 console.log(a * b);
