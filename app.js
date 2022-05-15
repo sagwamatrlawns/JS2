58a91898b20502df1faeb6e8f0ecffc5275b3a5b
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-15 11:17:18 +0900

    const, let사용법과 특징을 알고 var을 쓰지 말아야 하는 이유 알기

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Variables (변수))
 // JS에서는 변수명 사이에 공백을 넣을 수 없다.
 // 공백대신 다음 글자를 대문자로 입력해서 구분하는 것이 일반적이다.
+// 기본적으로 const를 쓰고 필요할 떄만 let을 쓰고, var은 쓰지 말자.
 
 const a = 5;
 const b = 2;
-const myName = "KJ";
+let myName = "KJ";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Kim Jun";
+
+console.log("your new name is " + myName);
