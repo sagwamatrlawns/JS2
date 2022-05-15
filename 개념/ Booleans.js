9bdd83e75913bf1b2d453ff72ed49d0268a6815d
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-15 11:32:47 +0900

    Booleans의 개념이해하기

---
+++ 개념/ Booleans.js
@@ -0,0 +1,12 @@
+// true, false
+// true : 켜져있음
+// false : 꺼져있음
+const amIFat = false;
+console.log(amIFat);
+
+// undefined, null
+// undefined : 메모리를 있지만 값이없느것
+// null : "비어있음"
+const amIfat = null;
+let something;
+console.log(something, amIFat);
