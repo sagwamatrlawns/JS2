53469f70518eaee91cdbabc18b0679a17ed5ca1b
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-18 19:12:40 +0900

    object와 function을 사용하여 계산기를 만들고console로 출력하기

---
+++ 개념/calculator.js
@@ -0,0 +1,27 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+
+  div: function (a, b) {
+    console.log(a / b);
+  },
+
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(5, 2);
+calculator.minus(5, 2);
+calculator.multi(5, 2);
+calculator.div(5, 2);
+calculator.power(5, 2);
