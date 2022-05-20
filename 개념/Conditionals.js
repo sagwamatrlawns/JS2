a1d6db1a973a6b2704848a752864832455f676e4
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-20 20:25:10 +0900

    :sparkles:::IsNaN
    isNaN는 입력값이 숫자라면 false를 반환하고 숫자가 아니라면 true값을 반환하는 function이다

--- 개념/Conditionals.js
+++ 개념/Conditionals.js
@@ -1,3 +1,8 @@
 const age = parseInt(prompt("How old are you?"));
+console.log(isNaN(age));
 
-console.log(age);
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else {
+  console.log("Thank you for writing your age.");
+}
