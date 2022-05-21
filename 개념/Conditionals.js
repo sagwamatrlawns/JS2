78c20274232ce97b2fc70b34e96cae3128e6a2ae
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-21 22:40:12 +0900

    :memo:::AND,OR
    true || true === true
    false || true === true
    true || false === true
    false || false === false
    
    true && true === true
    false && true === false
    true && false === false
    false && false === false

--- 개념/Conditionals.js
+++ 개념/Conditionals.js
@@ -1,8 +1,14 @@
 const age = parseInt(prompt("How old are you?"));
 console.log(isNaN(age));
 
-if (isNaN(age)) {
+if (isNaN(age) || age < 0) {
   console.log("Please write a number");
-} else {
-  console.log("Thank you for writing your age.");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 }
