b2223104c3ccacd81a278fa7449d964ac1023aec
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-30 13:47:11 +0900

    :sparkles: :: Event Two

--- 개념/Events.js
+++ 개념/Events.js
@@ -1,7 +1,19 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick() {
   title.style.color = "red";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
