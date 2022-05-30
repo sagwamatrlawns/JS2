f91688ebea603e453e31cace4adfd2f6501d2a65
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-05-30 14:38:50 +0900

    :sparkles:: :: More Events

--- 개념/Events.js
+++ 개념/Events.js
@@ -1,19 +1,43 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+console.dir(h1);
 
 function handleTitleClick() {
-  title.style.color = "red";
+  h1.style.color = "red";
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handlWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD!");
+}
+
+h1.addEventListener("click", handleTitleClick);
+// title.onclick = handleTitleClick;
+// 위의 코들르 이같이 줄일 수 있다.
+
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handlWindowOffline);
+window.addEventListener("online", handleWindowOnline);
