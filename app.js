f1431e4dc273ae7320869822ddb9c798107dad26
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-06-03 11:55:31 +0900

    :art: :: Events

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+//onst loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+  // 아무것도 하지 않음으로써, JS가 어떤 정보를 담은 채로 function을 호출한다 event object를 기본적으로 제공되는 function이 이 preventDefault이다.
+  event.preventDefault();
   //console.log(loginInput.value);
   //console.log("dial");
-  const username = loginInput.value;
-  console.log(username);
+  //const username = loginInput.value;
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+//loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
