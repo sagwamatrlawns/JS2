486f6505435544f93c875d828d6452dfacf36328
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-06-06 16:16:20 +0900

    :sparkles: Getting Username

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 //onst loginButton = document.querySelector("#login-form button");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   // 아무것도 하지 않음으로써, JS가 어떤 정보를 담은 채로 function을 호출한다 event object를 기본적으로 제공되는 function이 이 preventDefault이다.
@@ -10,7 +11,12 @@ function onLoginSubmit(event) {
   //console.log(loginInput.value);
   //console.log("dial");
   //const username = loginInput.value;
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log(username);
+  greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
