3550842e7b6c009c2345203ca38c12248b96980b
Kim jun <95758140+sagwamatrlawns@users.noreply.github.com>
2022-06-13 21:53:46 +0900

    :sparkles: :: todo list에 추가한 li를 삭제하는 함수 만들기

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// 버튼을 클릭하면 이 함수가 실행되고 event의 값을 가져온다.
+function deletToDo(event) {
+  // target은 클릭된 HTML element이다
+  // parentElement는 클릭된 element의 부모이다
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   // hmlt에 li를 생성함
   const li = document.createElement("li");
@@ -9,13 +17,24 @@ function paintToDo(newTodo) {
   // li안에 span을 생성함
   const span = document.createElement("span");
 
-  // span을 li안에다 넣음
-  li.appendChild(span);
-
   // newTodo에 저장된 input값을 span에 넣음
   span.innerText = newTodo;
 
-  // list에 li를 넣음
+  // html에 button을 생성해 준다.
+  const button = document.createElement("button");
+
+  // 버튼안의 텍스트
+  button.innerText = "❌";
+
+  // 버튼 클릭을 알려주는 기본함수
+  button.addEventListener("click", deletToDo);
+
+  // span을 li안에다 넣음
+  li.appendChild(span);
+
+  li.appendChild(button);
+
+  // list에 li
   toDoList.appendChild(li);
 }
 
